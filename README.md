1750a9cb6245a68e44eb6d89723683f71c4fcfd0
김철우 <kimchul8747@daum.net>
2023-12-04 21:48:03 +0900

    setting :: project setting

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# JuGanBbojjak-Frontend
+## JuGanBbojjak-Frontend
+
+주간 업무를 뽀짝뽀짝 취합해주는 서비스입니다.
---
+++ src/utils/theme/Theme.ts
@@ -0,0 +1,49 @@
+import { DefaultTheme } from "styled-components";
+
+const colors = {
+  Main: "#48CAF3",
+};
+
+const fonts = {
+  Bold32: `900 2rem 'Pretendard'`,
+  Bold28: `900 1.75rem 'Pretendard'`,
+  Bold24: `900 1.5rem 'Pretendard'`,
+  Bold20: `900 1.3rem 'Pretendard'`,
+  Bold16: `900 1rem 'Pretendard'`,
+  Bold12: `900 0.75rem 'Pretendard'`,
+  Semibold32: `600 2rem 'Pretendard'`,
+  Semibold28: `600 1.75rem 'Pretendard'`,
+  Semibold24: `600 1.5rem 'Pretendard'`,
+  Semibold20: `600 1.3rem 'Pretendard'`,
+  Semibold16: `600 1rem 'Pretendard'`,
+  Semibold12: `600 0.75rem 'Pretendard'`,
+  Medium32: `500 2rem 'Pretendard'`,
+  Medium28: `500 1.75rem 'Pretendard'`,
+  Medium24: `500 1.5rem 'Pretendard'`,
+  Medium20: `500 1.3rem 'Pretendard'`,
+  Medium16: `500 1rem 'Pretendard'`,
+  Medium14: `500 0.875rem 'Pretendard'`,
+  Medium12: `500 0.75rem 'Pretendard'`,
+  Regular32: `400 2rem 'Pretendard'`,
+  Regular28: `400 1.75rem 'Pretendard'`,
+  Regular24: `400 1.5rem 'Pretendard'`,
+  Regular20: `400 1.3rem 'Pretendard'`,
+  Regular16: `400 1rem 'Pretendard'`,
+  Regular12: `400 0.75rem 'Pretendard'`,
+  Light32: `300 2rem 'Pretendard'`,
+  Light28: `300 1.75rem 'Pretendard'`,
+  Light24: `300 1.5rem 'Pretendard'`,
+  Light20: `300 1.3rem 'Pretendard'`,
+  Light16: `300 1rem 'Pretendard'`,
+  Light12: `300 0.75rem 'Pretendard'`,
+};
+
+export type ColorsTypes = typeof colors;
+export type FontTypes = typeof fonts;
+
+const theme: DefaultTheme = {
+  colors,
+  fonts,
+};
+
+export default theme;
