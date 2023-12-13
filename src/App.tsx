17e647f8ddb3bdf32e9363ccd1f3fa32cebf8d22
김한비 <gksql2256@dsm.hs.kr>
2023-12-13 20:24:21 +0900

    feat :: writeReportPage publishing

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,22 @@
 import { QueryClientProvider, QueryClient } from "react-query";
-import GlobalStyle from "./style/GloblaStyle";
 import { ThemeProvider } from "styled-components";
 import theme from "./utils/theme/theme";
+import GlobalStyle from "./style/GlobalStyle"
+// import { BrowserRouter as Router } from "react-router-dom"; 
+// import WritingEventSchedulePage from "./pages/Pages/WritingEventSchedulePage";
+import WorkReportPage from "./pages/Pages/WorkReportPage";
 
 const queryClient = new QueryClient();
 
 function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <QueryClientProvider client={queryClient}>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={theme}>
+{/* <WritingEventSchedulePage/> */}
+<WorkReportPage/>
         <GlobalStyle />
-      </QueryClientProvider>
-    </ThemeProvider>
+      </ThemeProvider>
+    </QueryClientProvider>
   );
 }
 
---
+++ src/components/writhingPage/WorkReport.tsx
@@ -0,0 +1,97 @@
+import styled from "styled-components";
+import WorkReportComponent from "./WorkReportComponents";
+
+import Header from "../header";
+const WorkReport = () => {
+  return (
+    <>
+      <Header />
+      <Container>
+        <MainText>주간 업무 보고 작성</MainText>
+        <WorkReportComponent/>
+        <Button type="button">+</Button>
+        <ButtonWrapper>
+          <Cancelbutton>취소</Cancelbutton>
+          <Savebutton>저장</Savebutton>
+        </ButtonWrapper>
+      </Container>
+    </>
+  );
+};
+
+const Container = styled.div`
+  background: var(--White, #fff);
+  padding: 0 10%;
+`;
+const MainText = styled.div`
+  font-size: 32px;
+  font-weight: 700;
+  display: flex;
+  justify-content: center;
+  margin-bottom: 50px;
+`;
+
+const Button = styled.button`
+  border-radius: 8px;
+  background-color: #1a9fff;
+  display: flex;
+  height: 60px;
+  padding: 10px 0px;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  color: #ffff;
+  font-size: 45px;
+  border: none;
+  &:hover {
+    filter: brightness(1.25);
+    transition: all 0.3s;
+  }
+  margin-top: 50px;
+`;
+
+const Cancelbutton = styled.button`
+  border-radius: 8px;
+  background:  #d2d2d2;
+  display: flex;
+  width: 80px;
+  height: 40px;
+  padding: 15px 0px 16px 0px;
+  justify-content: center;
+  align-items: center;
+  color: #fff;
+  font-size: 18px;
+  font-weight: 700;
+  &:hover {
+    background: #1a9fff;
+  }
+  border: none;
+`;
+
+const Savebutton = styled.button`
+  border: none;
+  border-radius: 8px;
+  background: var(--Gray-300, #d2d2d2);
+  display: flex;
+  width: 80px;
+  height: 40px;
+  padding: 15px 0px 16px 0px;
+  justify-content: center;
+  align-items: center;
+  color: #fff;
+  font-size: 18px;
+  font-weight: 700;
+  &:hover {
+    background: #1a9fff;
+  }
+`;
+
+const ButtonWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  gap: 20px;
+  margin-top: 50px;
+  margin-bottom: 150px;
+`;
+
+export default WorkReport;
